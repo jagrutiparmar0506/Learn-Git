19817f04a176b16ae3801facfaf0206eec67a817
Jagruti Parmar <jagrutiparmar5696@gmail.com>
2022-05-27 21:48:28 +0530

    Third Commit

--- Sum.java
+++ Sum.java
@@ -1,12 +1,14 @@
 import java.util.*;
 public class Sum{
-    public staticvoid main(String[] args)
+    public static void main(String[] args){
          Scanner sc=new Scanner(System.in);
          System.out.println("Enter value of a: ");
          System.out.println("Enter value of b: ");
          int a=sc.nextInt();
          int b=sc.nextInt();
-         int a,b,c;
+         int c;
          c=a+b;
          System.out.println("Addition is: "+c);
+
+    }
 }
