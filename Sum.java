81516f290fe41956834940d3f4f3aff196b5b77c
Jagruti Parmar <jagrutiparmar5696@gmail.com>
2022-05-27 21:28:04 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum{
+    public staticvoid main(String[] args)
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter value of a: ");
+         System.out.println("Enter value of b: ");
+         int a=sc.nextInt();
+         int b=sc.nextInt();
+         int a,b,c;
+         c=a+b;
+         System.out.println("Addition is: "+c);
+}
